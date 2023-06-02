fcbf0994e9056f716e31fecae3f98da420803cda
Arslanamin404 <120253636+Arslanamin404@users.noreply.github.com>
2023-06-02 15:13:16 +0530

    leapYear.c

--- practceSet-2/leapYear.c
+++ practceSet-2/leapYear.c
@@ -1,6 +1,6 @@
 /* Any year is input through the keyboard.WAP to determine whether the year is leap year or not.
-condition: if year is divisible by 100 it is not a leap year unless Year is also divisible by 400.
-            if the year is not divisible by 100 it must be divisible by 4.
+condition: if the year is divisible by 4, 100 and 400, it is said to be leap year.
+exceptional cases of leap year are: 2000 was a leap year but the years 1700,1800 and 1900 were not.
 we will use nested if else statements
 
 */
@@ -10,7 +10,19 @@ int main()
     int year;
     printf("Enter the year: ");
     scanf("%d", &year);
-    
+            
+
+// using logical operator
+         if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+         {
+             printf("%d is a Leap Year.\n", year);
+         }
+         else
+         {
+             printf("%d is not a Leap Year.\n", year);
+         }
+            
+// using nested if else            
         if (year % 100 == 0)
         {
             if (year % 400 == 0)
