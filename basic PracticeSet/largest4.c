0919aae4b9fa51fc67b8f73761f8354e0722e5ef
Arslanamin404 <120253636+Arslanamin404@users.noreply.github.com>
2023-07-09 22:06:03 +0530

    Update largest4.c

--- basic PracticeSet/largest4.c
+++ basic PracticeSet/largest4.c
@@ -15,16 +15,16 @@ int main()
     scanf("%d", &num4);
 
     // try to make flow chart or rough sketch of code on notebook.
-    if (num1 > num2 && num1 > num3 && num1 > num4){
+    if (num1 >= num2 && num1 >= num3 && num1 >= num4){
         printf("%d is largest number",num1);
     }
-    else if(num2 > num3 && num2 > num4 && num2 > num1){
+    else if(num2 >= num3 && num2 >= num4 && num2 >= num1){
         printf("%d is largest number",num2);
     }
-    else if(num3 > num4 && num3 > num1 && num3 > num2){
+    else if(num3 >= num4 && num3 >= num1 && num3 >= num2){
         printf("%d is largest number",num3);
     }
-    else if(num4 > num1 && num4 > num2 && num4 > num3){
+    else if(num4 >= num1 && num4 >= num2 && num4 >= num3){
         printf("%d is largest number",num4);
     }
         return 0;
